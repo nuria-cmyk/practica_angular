a7c617ca965fcb9e2435bcee846e005f12924003
nuria <nuria.fernandez.val@gmail.com>
2024-11-16 13:36:10 +0100

    creación de las rutas posts y new

--- Blog/src/app/app.routes.ts
+++ Blog/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/posts' },
+    { path: 'posts', component: ListaPostsComponent },
+    { path: 'new', component: FormularioComponent }
+];
