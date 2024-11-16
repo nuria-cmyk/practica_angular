ef4180006e9aa4521aa573b6c2e81543c3d96a8b
nuria <nuria.fernandez.val@gmail.com>
2024-11-16 13:23:13 +0100

    creación de los componentes ListaPosts y Formulario

---
+++ Blog/src/app/components/lista-posts/lista-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-posts.component.html',
+  styleUrl: './lista-posts.component.css'
+})
+export class ListaPostsComponent {
+
+}
